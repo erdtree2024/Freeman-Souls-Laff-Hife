0d4041beb66517d3c6b3312de811df4871b937b4
HtchHiker42 <galaxyrgms@gmail.com>
2023-07-28 16:39:05 -0400

    added camera

--- game.py
+++ game.py
@@ -27,7 +27,7 @@ CRATE_SCALING = 0.2
 # Movement speed of player, in pixels per frame
 PLAYER_MOVEMENT_SPEED = 5
 
-GRAVITY = .16
+GRAVITY = .3
 PLAYER_JUMP_SPEED = 7
 
 class MyGame(arcade.Window):
@@ -40,7 +40,8 @@ class MyGame(arcade.Window):
         # Call the parent class and set up the window
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
-
+        # A Camera that can be used for scrolling the screen
+        self.camera = None
         # These are 'lists' that keep track of our sprites. Each sprite should
 
         # go into a list.
@@ -56,6 +57,8 @@ class MyGame(arcade.Window):
 
 
     def setup(self):
+        # Set up the Camera
+        self.camera = arcade.Camera(self.width, self.height)
 
         """Set up the game here. Call this function to restart the game."""
 
@@ -129,7 +132,7 @@ class MyGame(arcade.Window):
 
     def on_draw(self):
         """Render the screen."""
-
+        self.camera.use()
         # Clear the screen to the background color
         self.clear()
         self.scene.draw()
@@ -154,11 +157,45 @@ class MyGame(arcade.Window):
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.player_sprite.change_x = 0
 
+    def center_camera_to_player(self):
+
+        screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
+
+        screen_center_y = self.player_sprite.center_y - (
+
+            self.camera.viewport_height / 2
+
+        )
+
+
+
+        # Don't let camera travel past 0
+
+        if screen_center_x < 0:
+
+            screen_center_x = 0
+
+        if screen_center_y < 0:
+
+            screen_center_y = 0
+
+        player_centered = screen_center_x, screen_center_y
+
+
+
+        self.camera.move_to(player_centered)
+
+
     def on_update(self, delta_time):
         """Movement and game logic"""
 
         # Move the player with the physics engine
         self.physics_engine.update()
+
+
+        # Position the camera
+
+        self.center_camera_to_player()
 
 def main():
     """Main function"""
